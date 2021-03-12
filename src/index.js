e3f558f23b64acb527732e302eb8bc832d942618
stavrovadv <stavrovadaria97@gmail.com>
2021-03-13 01:34:30 +0500

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let elementsStr = str.split('');
+    let stack = [];
+    let arrBracketsConfig = bracketsConfig.toString().split(',');
+
+    if (elementsStr.length % 2 !== 0) return false;
+    for (let i = 0; i < elementsStr.length; i++) {
+        let indStr = arrBracketsConfig.indexOf(elementsStr[i]);
+        if (indStr % 2 === 0) {
+            stack.push(elementsStr[i]);
+            if (elementsStr[i] === arrBracketsConfig[indStr + 1]) {
+                if (stack[stack.length - 2] === elementsStr[i]) {
+                    stack.pop();
+                    stack.pop();
+                }
+                if (stack[stack.length - 3] === elementsStr[i]) {
+                    return false;
+                }
+            }
+        } else {
+            let indElemLastStack = arrBracketsConfig.indexOf(stack.pop());
+            if (elementsStr[i] !== arrBracketsConfig[indElemLastStack + 1]) return false;
+        }
+    }
+    return true;
 }
